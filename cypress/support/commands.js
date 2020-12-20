360e70900706a3cbcb27271407905f47935332b3
thamyresmoraes <thamyresmoraes@live.com>
2020-12-19 22:09:58 -0300

    fix indentation commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,7 +27,7 @@
 
 Cypress.Commands.add("fillMandatoryFields", data =>{
     cy.get("#first-name").type(data.firstName);
-	cy.get("#last-name").type(data.lastName);
-	cy.get("#email").type(data.email);
+    cy.get("#last-name").type(data.lastName);
+    cy.get("#email").type(data.email);
     cy.get("#agree").check()
 })
